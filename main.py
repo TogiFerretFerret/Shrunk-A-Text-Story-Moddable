0b07c50cbd66a27dff5dc3c185c0cbfe1969c8ca
TogiFerretFerret <114247361+TogiFerretFerret@users.noreply.github.com>
2023-11-08 16:05:07 +0000

    t

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import os
 import forge.mod
 import pluggy
+from replit import db
+from colored import fore, style
+import fkeycapture as fkey  # Better menus 😀
+import sys
+import termios
+import tty
+from pyfiglet import Figlet
+import time
 
 pm = pluggy.PluginManager("shrunk")
 pm.add_hookspecs(forge.mod.DefaultMod)
@@ -26,3 +34,22 @@ for mod in mods_to_load:
 userStrDelay = 0.04
 numDeaths = 0
 bestFriend = ""
+# DB Stuff
+db.db_url = "https://Shrunk-Database.tg101.repl.co"  # type: ignore
+os.environ["REPL_DB_URL"] = "https://Shrunk-Database.tg101.repl.co"
+# Next line is so one user's data doesn't overwrite everyone else's
+username = os.environ["REPL_OWNER"]
+# Disclaimer code
+def readDisclaim():
+  print(
+      f"{fore(1)}Do you want to read the disclaimer before proceeding? (y/n)\n >{style(0)}"
+  )
+  # disclaimerRead = str(fkey.getchars(chars=["y", "n"]))
+  disclaimerRead = "n" # For Testing
+  if disclaimerRead == "y":
+      os.system("clear")
+      disclaimer()
+      mainMenu()
+  else:
+      os.system("clear")
+      mainMenu()
